fd3709fede8cea1c712f0033b01ad5962dd7be74
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-10-02 16:23:51 +0300

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Sylvan  on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
