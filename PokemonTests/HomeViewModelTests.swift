550cfd70b3d443557f4175f28e0945b457b60353
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-10-02 16:42:27 +0300

    homepage: add unit tests

---
+++ PokemonTests/HomeViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by Sylvan  on 02/10/2025.
+//
+
+import Testing
+
+struct PokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
