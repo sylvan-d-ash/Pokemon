d8daad6c7f5d76003911fd4ecca37e654abe7063
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-10-02 20:59:37 +0300

    background: add an extension of UIImageColors to convert to SwiftUI.Color

---
+++ Pokemon/Extensions/UIImageColors.swift
@@ -0,0 +1,15 @@
+//
+//  UIImageColors.swift
+//  Pokemon
+//
+//  Created by Sylvan  on 02/10/2025.
+//
+
+import SwiftUI
+import UIImageColors
+
+extension UIImageColors {
+    var primaryColor: Color { Color(uiColor: primary) }
+    var secondaryColor: Color { Color(uiColor: secondary) }
+    var backgroundColor: Color { Color(uiColor: background) }
+}
